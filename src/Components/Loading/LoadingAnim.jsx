723bdd3411ffbab73d7bef87a092156daf75f34c
eduardo_gora <eduardogoracedi@outlook.com>
2023-10-08 21:01:57 -0600

    animationComplete

---
+++ src/Components/Loading/LoadingAnim.jsx
@@ -0,0 +1,486 @@
+import "./LoadingAnim.css";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import {Routes, Route, useNavigate} from "react-router-dom"
+
+const draw = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i) => {
+        const delay = 1 + i * 0.5;
+        return {
+            pathLength: 1,
+            opacity: 1,
+            transition: {
+                pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+                opacity: { delay, duration: 0.01 }
+            }
+        };
+    }
+};
+
+export default function LoadingAnim() {
+    //Button
+
+    const [showButton, setShowButton] = useState(false);
+
+    function handleButtonClick(){
+        console.log(1)
+        console.log(showButton)
+    }
+
+    //Animation
+    const stroke = 15;
+    var numLetra = 0;
+    return (
+        <div>
+        <motion.svg
+            width="1000"
+            height="800"
+            viewBox="0 0 1000 800"
+            initial="hidden"
+            animate="visible"
+            onAnimationComplete={() => setShowButton(true)}
+        >
+        {/*G*/ numLetra = 0}
+
+            <motion.line
+                x1="80"
+                y1="270"
+                x2="150"
+                y2="270"
+                stroke="#00cc88"
+                variants={draw}
+                custom={2}
+            />
+            <motion.line
+                x1="80"
+                y1="100"
+                x2="200"
+                y2="100"
+                stroke="#00cc88"
+                variants={draw}
+                custom={2}
+            />
+            <motion.line
+                x1="80"
+                y1="100"
+                x2="80"
+                y2="270"
+                stroke="#00cc88"
+                variants={draw}
+                custom={2}
+            />
+            <motion.line
+                x1="200"
+                y1="300"
+                x2="200"
+                y2="225"
+                stroke="#00cc88"
+                variants={draw}
+                custom={2}
+            />
+            <motion.line
+                x1="150"
+                y1="225"
+                x2="200"
+                y2="225"
+                stroke="#00cc88"
+                variants={draw}
+                custom={2}
+            />
+            <motion.line
+                x1="80"
+                y1="300"
+                x2="200"
+                y2="300"
+                stroke="#00cc88"
+                variants={draw}
+                custom={2}
+            />
+            {/*R*/ numLetra = 1}
+            <motion.line
+                x1={215 + numLetra * stroke}
+                y1="100"
+                x2={300 + numLetra * stroke}
+                y2="100"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={200 + numLetra * stroke}
+                y1="200"
+                x2={300 + numLetra * stroke}
+                y2="200"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={200 + numLetra * stroke}
+                y1="100"
+                x2={200 + numLetra * stroke}
+                y2="300"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={300 + numLetra * stroke}
+                y1="100"
+                x2={300 + numLetra * stroke}
+                y2="200"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={200 + numLetra * stroke}
+                y1="200"
+                x2={300 + numLetra * stroke}
+                y2="300"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+            <motion.line
+                x1={200 + numLetra * stroke}
+                y1="100"
+                x2={300 + numLetra * stroke}
+                y2="100"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={200 + numLetra * stroke}
+                y1="200"
+                x2={300 + numLetra * stroke}
+                y2="200"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={200 + numLetra * stroke}
+                y1="100"
+                x2={200 + numLetra * stroke}
+                y2="300"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={300 + numLetra * stroke}
+                y1="100"
+                x2={300 + numLetra * stroke}
+                y2="200"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+
+            <motion.line
+                x1={200 + numLetra * stroke}
+                y1="200"
+                x2={300 + numLetra * stroke}
+                y2="300"
+                stroke="#fcba03"
+                variants={draw}
+                custom={3}
+            />
+            {/*A*/ numLetra = 2}
+            <motion.line
+                x1={350 + numLetra * stroke}
+                y1="100"
+                x2={300 + numLetra * stroke}
+                y2="300"
+                stroke="#fc03db"
+                variants={draw}
+                custom={4}
+            />
+            <motion.line
+                x1={350 + numLetra * stroke}
+                y1="100"
+                x2={400 + numLetra * stroke}
+                y2="300"
+                stroke="#fc03db"
+                variants={draw}
+                custom={4}
+            />
+            <motion.line
+                x1={300 + numLetra * stroke}
+                y1="200"
+                x2={400 + numLetra * stroke}
+                y2="200"
+                stroke="#fc03db"
+                variants={draw}
+                custom={4}
+            />
+            {/*P*/ numLetra = 3}
+            <motion.line
+                x1={400 + numLetra * stroke}
+                y1="100"
+                x2={500 + numLetra * stroke}
+                y2="100"
+                stroke="#4791ff"
+                variants={draw}
+                custom={5}
+            />
+            <motion.line
+                x1={400 + numLetra * stroke}
+                y1="200"
+                x2={500 + numLetra * stroke}
+                y2="200"
+                stroke="#4791ff"
+                variants={draw}
+                custom={5}
+            />
+            <motion.line
+                x1={400 + numLetra * stroke}
+                y1="100"
+                x2={400 + numLetra * stroke}
+                y2="300"
+                stroke="#4791ff"
+                variants={draw}
+                custom={5}
+            />
+            <motion.line
+                x1={500 + numLetra * stroke}
+                y1="100"
+                x2={500 + numLetra * stroke}
+                y2="200"
+                stroke="#4791ff"
+                variants={draw}
+                custom={5}
+            />
+            {/*H*/ numLetra = 4}
+            <motion.line
+                x1={500 + numLetra * stroke}
+                y1="200"
+                x2={600 + numLetra * stroke}
+                y2="200"
+                stroke="#00ffb7"
+                variants={draw}
+                custom={6}
+            />
+            <motion.line
+                x1={500 + numLetra * stroke}
+                y1="100"
+                x2={500 + numLetra * stroke}
+                y2="300"
+                stroke="#00ffb7"
+                variants={draw}
+                custom={6}
+            />
+            <motion.line
+                x1={600 + numLetra * stroke}
+                y1="100"
+                x2={600 + numLetra * stroke}
+                y2="300"
+                stroke="#00ffb7"
+                variants={draw}
+                custom={6}
+            />
+            {/*C*/ numLetra = 0}
+            <motion.line
+                x1={150 + numLetra * stroke}
+                y1={300 + stroke}
+                x2={250 + numLetra * stroke}
+                y2={300 + stroke}
+                stroke="#00ffb7"
+                variants={draw}
+                custom={6}
+            />
+            <motion.line
+                x1={150 + numLetra * stroke}
+                y1={300 + stroke}
+                x2={150 + numLetra * stroke}
+                y2={500 + stroke}
+                stroke="#00ffb7"
+                variants={draw}
+                custom={6}
+            />
+            <motion.line
+                x1={150 + numLetra * stroke}
+                y1={500 + stroke}
+                x2={250 + numLetra * stroke}
+                y2={500 + stroke}
+                stroke="#00ffb7"
+                variants={draw}
+                custom={6}
+            />
+            {/*O*/ numLetra = 1}
+            <motion.line
+                x1={250 + numLetra * stroke}
+                y1={300 + stroke}
+                x2={350 + numLetra * stroke}
+                y2={300 + stroke}
+                stroke="#405178"
+                variants={draw}
+                custom={7}
+            />
+            <motion.line
+                x1={250 + numLetra * stroke}
+                y1={500 + stroke}
+                x2={350 + numLetra * stroke}
+                y2={500 + stroke}
+                stroke="#405178"
+                variants={draw}
+                custom={7}
+            />
+            <motion.line
+                x1={250 + numLetra * stroke}
+                y1={300 + stroke}
+                x2={250 + numLetra * stroke}
+                y2={500 + stroke}
+                stroke="#405178"
+                variants={draw}
+                custom={7}
+            />
+            <motion.line
+                x1={350 + numLetra * stroke}
+                y1={500 + stroke}
+                x2={350 + numLetra * stroke}
+                y2={300 + stroke}
+                stroke="#405178"
+                variants={draw}
+                custom={7}
+            />
+            {/*D*/ numLetra = 2}
+            <motion.line
+                x1={350 + numLetra * stroke}
+                y1={300 + stroke}
+                x2={350 + numLetra * stroke}
+                y2={500 + stroke}
+                stroke="#ad1066"
+                variants={draw}
+                custom={8}
+            />
+            <motion.line
+                x1={450 + numLetra * stroke}
+                y1={400 + stroke}
+                x2={350 + numLetra * stroke}
+                y2={300 + stroke}
+                stroke="#ad1066"
+                variants={draw}
+                custom={8}
+            />
+            <motion.line
+                x1={450 + numLetra * stroke}
+                y1={400 + stroke}
+                x2={350 + numLetra * stroke}
+                y2={500 + stroke}
+                stroke="#ad1066"
+                variants={draw}
+                custom={8}
+            />
+            {/*E*/ numLetra = 3}
+            <motion.line
+                x1={450 + numLetra * stroke}
+                y1={300 + stroke}
+                x2={550 + numLetra * stroke}
+                y2={300 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={9}
+            />
+            <motion.line
+                x1={450 + numLetra * stroke}
+                y1={400 + stroke}
+                x2={550 + numLetra * stroke}
+                y2={400 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={9}
+            />
+            <motion.line
+                x1={450 + numLetra * stroke}
+                y1={500 + stroke}
+                x2={550 + numLetra * stroke}
+                y2={500 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={9}
+            />
+
+            {/*BY EN*/}
+            <motion.line
+                x1={800 + numLetra * stroke}
+                y1={600 + stroke}
+                x2={825 + numLetra * stroke}
+                y2={600 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={10}
+                style={{strokeWidth: "5px"}}
+            />
+            <motion.line
+                x1={800 + numLetra * stroke}
+                y1={625 + stroke}
+                x2={825 + numLetra * stroke}
+                y2={625 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={10}
+                style={{strokeWidth: "5px"}}
+            />
+            <motion.line
+                x1={800 + numLetra * stroke}
+                y1={650 + stroke}
+                x2={825 + numLetra * stroke}
+                y2={650 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={10}
+                style={{strokeWidth: "5px"}}
+            />
+
+            <motion.line
+                x1={825 + numLetra * stroke}
+                y1={600 + stroke}
+                x2={825 + numLetra * stroke}
+                y2={650 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={10}
+                style={{strokeWidth: "5px"}}
+            />
+            <motion.line
+                x1={825 + numLetra * stroke}
+                y1={650 + stroke}
+                x2={850 + numLetra * stroke}
+                y2={600 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={10}
+                style={{strokeWidth: "5px"}}
+            />
+            <motion.line
+                x1={850 + numLetra * stroke}
+                y1={600 + stroke}
+                x2={850 + numLetra * stroke}
+                y2={650 + stroke}
+                stroke="#ff5100"
+                variants={draw}
+                custom={10}
+                style={{strokeWidth: "5px"}}
+            />
+
+        </motion.svg>
+
+        {showButton && (
+        <button onClick={handleButtonClick}>Let´s go</button>
+      ) }
+        </div>
+
+
+    );
+}
---
+++ src/Views/MainPage.jsx
@@ -0,0 +1,8 @@
+import {motion} from "framer-motion"
+import LoadingAnim from "../Components/Loading/LoadingAnim"
+
+export default function Loading() {
+    return(
+        <h1>Siuuuu</h1>
+    )
+}
