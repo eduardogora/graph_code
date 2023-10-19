8049725c34c9b4515714ed8e8ac57ff92defd59a
eduardo_gora <eduardogoracedi@outlook.com>
2023-10-19 01:31:55 -0600

    changesInParallax

--- src/Components/Parallax/paralax.jsx
+++ src/Components/Parallax/paralax.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import './Paralax.css';
+import './parallax.css';
 import { Button } from "antd"
 import { Link, Element } from 'react-scroll';
 import {  DownOutlined, UpOutlined } from '@ant-design/icons';
 
-const Parallax = ({ image, caption, parNum }) => {
+const Parallax = ({ image, parNum }) => {
   return (
     <Element name= {"par" + parNum}>
     <div className="parallax-container">
       <div className="parallax" style={{ backgroundImage: `url(${image})` }}></div>
-      <div className="triangle-button left"></div>
-      <div className="triangle-button right"></div>
 
 
       <Link
-          to= {parNum == 2 ? "par0" : "par" + (parNum + 1)} // Este debe ser el identificador único de la siguiente sección
+          to= {parNum == 2 ? "par0" : "par" + (parNum + 2)} // Este debe ser el identificador único de la siguiente sección
           spy={true}
           smooth={true}
           offset={-70}
           duration={500}
           className="scroll-down-button"
         >
-          <Button> {parNum == 2 ? <UpOutlined/> : <DownOutlined/>} </Button>
+          {parNum !== 1 ? <Button> {parNum == 2 ? <UpOutlined/> : <DownOutlined/>} </Button> : null}
+          
         </Link>
-
-      <div className="caption">{caption}</div>
     </div>
     </Element>
   );
--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -7,7 +7,7 @@ import img from "../../Resources/Images/persona.png"
 import Parallax1 from "../../Resources/Images/Paralax1.png"
 import Parallax2 from "../../Resources/Images/Paralax2.png"
 import Projects from "../../Views/Projects";
-import Parallax from "../../Components/Parallax/paralax";
+import Parallax from "../../Components/Parallax/Paralax";
 
 import headerHome from "../../Resources/Images/headerHome.png"
 import { right } from "@popperjs/core";
@@ -19,9 +19,9 @@ export default function Home({data},{handleSelection}){
         <div >
             <Header></Header>
             <div>
-                <Parallax image={Parallax1}/>
-                <Parallax image={Parallax1}/>
-                <Parallax image={Parallax2}/>
+                <Parallax image={Parallax1} parNum = {0}/>
+                <Parallax image={Parallax1} parNum = {1}/>
+                <Parallax image={Parallax2} parNum = {2}/>
             </div>
         </div>
     )
